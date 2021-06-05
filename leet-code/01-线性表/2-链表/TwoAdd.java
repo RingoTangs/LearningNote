029c44696f1a9c9ba03aee434a8dedbb5921f479
RingoTangs <1466637477@qq.com>
2021-06-05 20:20:57 +0800

    两数相加

--- leet-code/01-线性表/2-链表/TwoAdd.java
+++ leet-code/01-线性表/2-链表/TwoAdd.java
@@ -6,10 +6,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
- * 两数相加: 给出两个非空的链表用来表示两个非负的整数。
+ * leet-code(2) 两数相加: 给出两个非空的链表用来表示两个非负的整数。
  * 其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
  * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
- * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。 
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
  *
  * <p>
  * 详情请看 https://github.com/MisterBooo/LeetCodeAnimation
@@ -43,14 +43,19 @@ public class TwoAdd {
             p1 = p1 != null ? p1.getNext() : null;
             p2 = p2 != null ? p2.getNext() : null;
         }
+
+        // 注意: 做题时这里没考虑到
+        if (carried > 0)
+            cur.setNext(new Node(carried, null));
+
         Node ret = dummyHead.getNext();
         dummyHead = null;       // 将引用指向为null等待GC回收
         return ret;
     }
 
     public static void main(String[] args) {
-        Node p1 = new Node(2, new Node(4, new Node(3, null)));
-        Node p2 = new Node(5, new Node(6, null));
+        Node p1 = new Node(9, new Node(9, new Node(9, null)));
+        Node p2 = new Node(9, new Node(9, null));
         Node ret = addTwoNumbers(p1, p2);
         while (ret != null) {
             System.out.print(ret.getValue() + "\t");
