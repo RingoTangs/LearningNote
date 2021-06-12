175bda939bc03fe90cd2ccfba0ea20a2ec4dfe5b
RingoTangs <1466637477@qq.com>
2021-06-12 21:42:18 +0800

    旋转图像

---
+++ leet-code/01-线性表/1-数组/MatrixRotate.java
@@ -0,0 +1,35 @@
+package array;
+
+/**
+ * leet-code(48) 旋转图像
+ * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+ * 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+ *
+ * @author Ringo
+ * @date 2021/6/11 22:15
+ */
+public class MatrixRotate {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {5, 1, 9, 11},
+                {2, 4, 8, 10},
+                {13, 3, 6, 7},
+                {15, 14, 12, 16}
+        };
+        rotate(matrix);
+    }
+
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < (n + 1) / 2; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+}
